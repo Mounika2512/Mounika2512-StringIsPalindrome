ca66c25f74e7c9c4bf01b6369cac7e53aec5e86d
Mounika2512 <mounikasaviti25@gmail.com>
2024-04-05 19:51:11 +0530

    stringispalindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,4 +1,3 @@
-
 public class Palindrome {
     /**
      * Palindromes are words that are the same forwards as they are backwards - eg, 'bob', 'racecar'.
@@ -12,6 +11,18 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
-    }
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                 return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+     }
 }
